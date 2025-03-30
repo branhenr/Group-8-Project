75316c12951b1f2ebed6ae1a4b377855ef55c5d2
Brandon Vazquez-Henriquez <brandon.vazquez.henriquez@gmail.com>
2025-03-30 17:07:19 -0400

    Pushing files for backend API for chef.

---
+++ Fork-ShareBackEndAPI/src/main/java/com/Group8/Fork/ShareBackEndAPI/Application.java
@@ -0,0 +1,13 @@
+package com.Group8.Fork.ShareBackEndAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
