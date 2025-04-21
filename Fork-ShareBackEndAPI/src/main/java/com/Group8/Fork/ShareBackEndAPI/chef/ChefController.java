84162bf41d3f43628b2c9923bb7a56cc8b80c8e4
Brandon Vazquez-Henriquez <brandon.vazquez.henriquez@gmail.com>
2025-04-21 19:21:10 -0400

    Added dependecies

--- Fork-ShareBackEndAPI/src/main/java/com/Group8/Fork/ShareBackEndAPI/chef/ChefController.java
+++ Fork-ShareBackEndAPI/src/main/java/com/Group8/Fork/ShareBackEndAPI/chef/ChefController.java
@@ -3,10 +3,13 @@ package com.Group8.Fork.ShareBackEndAPI.chef;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
 
 
-@RestController
+//@RestController
+@Controller
 @RequestMapping("/chefs")
 public class ChefController {
 
@@ -15,20 +18,33 @@ public class ChefController {
 
     //return all chefs
     @GetMapping("/all")
-    public Object getAllChefs() {
-        return new ResponseEntity<>(service.getAllChefs(), HttpStatus.OK);
+    public Object getAllChefs(Model model) {
+        //return new ResponseEntity<>(service.getAllChefs(), HttpStatus.OK);
+        model.addAttribute("chefList", service.getAllChefs());
+        model.addAttribute("title", "All Chefs");
+        return "chef-list";
     }
 
     //return a chef by Id
     @GetMapping("/{chefId}")
-    public Object getOneChef(@PathVariable int chefId){
-        return new ResponseEntity<>(service.getChefById(chefId), HttpStatus.OK);
+    public Object getOneChef(@PathVariable int chefId, Model model){
+        //return new ResponseEntity<>(service.getChefById(chefId), HttpStatus.OK);
+        model.addAttribute("chef", service.getChefById(chefId));
+        model.addAttribute("title", "Chef #: " + chefId);
+        return "chef-details";
     }
 
     //return chef by name
     @GetMapping("/name")
     public Object getChefsByName(@RequestParam(name = "search", defaultValue = "") String search){
         return new ResponseEntity<>(service.getChefsByName(search), HttpStatus.OK);
+    }
+
+    //show form to create a profile for chef
+    @GetMapping("/createForm")
+    public String showCreateForm(Model model){
+        Chef chef = new Chef();
+
     }
 
 
